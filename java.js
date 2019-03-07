bfe84c38fa708fcb7c8deabdd1dcd622922e2ac6
chaitratotad <47623044+chaitratotad@users.noreply.github.com>
2019-03-07 12:42:53 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,89 @@
+document.write("welcome to pu"+"<br/><br/>");
+for(i=0;i<10;i++)
+document.write("my value of i is:"+ i+"<br/>");
+
+var br="<br/>";
+
+var a=10;
+document.write(typeof(a)+"<br/>");
+
+var d=new Date();
+document.write(typeof(d)+"<br/>");
+document.write(d+"<br/>");
+document.write(br);
+
+var s="pink";document.write("s:");
+document.write(typeof(s)+"<br/>");
+
+var b=true;
+document.write("b:");
+document.write(typeof(b)+"<br/>");
+
+var b2;
+document.write("b2:");
+document.write(typeof(b2)+"<br/>");
+
+var d1=null;document.write("d1:");
+document.write(typeof(d1)+"<br/>");
+document.write(br);
+var a=33;
+var b=10;
+var c="test";
+document.write("a+b=");
+result=a+b;
+document.write(result+"<br/>");
+
+var a=33;
+var b=10;
+var c="test";
+document.write("a-b=");
+result=a-b;
+document.write(result+"<br/>");
+
+var a=33;
+var b=10;
+var c="test";
+document.write("a*b=");
+result=a*b;
+document.write(result+"<br/>");
+
+var a=33;
+var b=10;
+var c="test";
+document.write("a/b=");
+result=a/b;
+document.write(result+"<br/>");
+
+var a=33;
+var b=10;
+var c="test";
+document.write("a%b=");
+result=a%b;
+document.write(result+"<br/>");
+
+var a=33;
+var b=10;
+var c="test";
+document.write("a+b+c=");
+result=a+b+c;
+document.write(result+"<br/>");
+
+a=++a;
+document.write("++a=");
+result=++a;
+document.write(result+"<br/>");
+
+b=--b;
+document.write("--b=");
+result=--b;
+document.write(result+"<br/>");
+document.write(br);
+
+result=(typeof c =="string"?"c is string":"c is number");
+document.write(result+"<br/>");
+document.write(br);
+
+result=(typeof a =="string"?"a is string":"a is number");
+document.write(result+"<br/>");
+document.write(br);
+
